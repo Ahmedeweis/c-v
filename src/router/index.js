800b01cce7a7f9dd5b232b32945afe9bf199bb42
Ahmedeweis <Ahmedeweiso@gmail.com>
2024-07-07 23:42:48 +0300

    add

--- src/router/index.js
+++ src/router/index.js
@@ -23,14 +23,14 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/CvView.vue')
   },
-  {
-    path: '/Fashoin',
-    name: 'Fashoin',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../projects/Fashoinn/FashoinView.vue')
-  },
+  // {
+  //   path: '/Fashoin',
+  //   name: 'Fashoin',
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: () => import(/* webpackChunkName: "about" */ '../projects/Fashoinn/FashoinView.vue')
+  // },
   {
     path: '/Aqarbot',
     name: 'Aqarbot',
